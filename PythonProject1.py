bad395c5ac42c5ae6fa60cb72249de33a5d3769c
СтепашкаУспевашка <fuker1993@yandex.ru>
2023-01-21 19:18:09 +0300

    create file PythonProject1

--- PythonProject1.py
+++ PythonProject1.py
@@ -1,2 +1,32 @@
-"adassgdsfdsff"
-"что нибудь еще добавим"
+per1=100
+per2=100.12345
+print(per1+per2)
+print(per1*per2)
+print(per1-per2)
+print(per1/per2)
+
+
+
+
+per3="https://www.google.ru/"
+print(per3)
+per4="Интерестный проект созданный в Беларуси https://skyway.link-stats.site/technology/?partner_id=580125&lang=ru"
+print(per4)
+
+
+
+
+a=100<10
+b=10<100
+print(a>b)
+a=100<10
+b=10<100
+print(a<b)
+
+
+a=100
+b=20
+if a>b:
+    print("a больше чем b")
+else:
+    print("b больше чем a")
